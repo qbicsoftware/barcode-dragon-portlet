8ba81d0099c31ecd119fca96fd53a3349639c78b
sven1103 <sven.fillinger@gmail.com>
2017-06-12 16:37:27 +0200

    New linking of widgetset

--- src/main/java/life/qbic/barcoder/QbicbarcodedragonUI.java
+++ src/main/java/life/qbic/barcoder/QbicbarcodedragonUI.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
