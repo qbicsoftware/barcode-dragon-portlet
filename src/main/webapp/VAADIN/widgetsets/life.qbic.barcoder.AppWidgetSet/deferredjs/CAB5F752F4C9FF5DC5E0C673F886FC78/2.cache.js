56524699c4181d03717f2973c61a9bc3924ac4e5
sven1103 <sven.fillinger@gmail.com>
2017-06-12 15:16:13 +0200

    Clean WidgetSet

--- src/main/webapp/VAADIN/widgetsets/life.qbic.barcoder.AppWidgetSet/deferredjs/CAB5F752F4C9FF5DC5E0C673F886FC78/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.life_qbic_barcoder_AppWidgetSet.runAsyncCallback2('Ofb(1659,1,oge);_.vc=function ulc(){E5b((!x5b&&(x5b=new J5b),x5b),this.a.d)};K9d(Th)(2);\n//# sourceURL=life.qbic.barcoder.AppWidgetSet-2.js\n')
